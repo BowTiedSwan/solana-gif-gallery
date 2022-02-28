7563978b9e6413653e529fb9a5e44af38eaf6aab
BowTiedSwan <86532747+BowTiedSwan@users.noreply.github.com>
2022-02-28 16:17:01 +0000

    Add solana wallet extension listener

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'bowtiedswan';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  // Function to detect if Phantom wallet is connected
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+        }
+      } else {
+        alert('Solana object not found! Get a Phantom Wallet');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  // Once our compnent mounts, lets check if we have a connected wallet
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+  
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">GIF_Block</p>
           <p className="sub-text">
-            View your GIF collection in the metaverse ✨
+            Your on-chain GIF gallery
           </p>
         </div>
         <div className="footer-container">
@@ -23,7 +48,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
